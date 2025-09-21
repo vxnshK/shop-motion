87567d653df1ae9da26c41336ec956a158e2e78e
vxnshK <vkapoor0112@gmail.com>
2025-09-21 20:06:24 +0530

    linter

--- components/feed/Sidebar/LogOutButton.tsx
+++ components/feed/Sidebar/LogOutButton.tsx
@@ -1,16 +1,22 @@
-"use client"; // if using Next.js App Router
+'use client'; // if using Next.js App Router
 
-import React from "react";
-import { motion } from "motion/react";
-import { LogOut } from "lucide-react";
+import React from 'react';
+import { motion } from 'motion/react';
+import { LogOut } from 'lucide-react';
 
 function LogOutButton() {
   return (
     <motion.div
       initial={{ opacity: 0, y: 40 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.8, ease: "easeOut", type: "spring", stiffness: 80, damping: 15  }}
-      className="flex gap-[8px] justify-center items-center w-full text-md font-medium bg-gray-200 text-black hover:bg-rose-500 hover:text-white cursor-pointer px-6 py-2 rounded-3xl"
+      transition={{
+        duration: 0.8,
+        ease: 'easeOut',
+        type: 'spring',
+        stiffness: 80,
+        damping: 15,
+      }}
+      className='flex gap-[8px] justify-center items-center w-full text-md font-medium bg-gray-200 text-black hover:bg-rose-500 hover:text-white cursor-pointer px-6 py-2 rounded-3xl'
     >
       <LogOut size={13} />
       <span>Log out</span>
--- components/feed/Sidebar/SidebarItems.tsx
+++ components/feed/Sidebar/SidebarItems.tsx
@@ -1,4 +1,4 @@
-"use client"; // if in Next.js App Router
+'use client'; // if in Next.js App Router
 
 const SidebarItems = () => {
   return (
@@ -14,16 +14,16 @@ export default SidebarItems;
 
 // ==========================================================================
 
-import { motion } from "motion/react";
-import { Zap, Compass, ShoppingCart, Gift, Lightbulb } from "lucide-react";
+import { motion } from 'motion/react';
+import { Zap, Compass, ShoppingCart, Gift, Lightbulb } from 'lucide-react';
 
 export const NavItems = () => {
   const navItemLinks = [
-    { name: "Popular Products", icon: <Zap size={15} />, active: false },
-    { name: "Explore Now", icon: <Compass size={15} />, active: true },
-    { name: "Clothing Store", icon: <ShoppingCart size={15} />, active: false },
-    { name: "Gifts and Living", icon: <Gift size={15} />, active: false },
-    { name: "Inspiration", icon: <Lightbulb size={15} />, active: false },
+    { name: 'Popular Products', icon: <Zap size={15} />, active: false },
+    { name: 'Explore Now', icon: <Compass size={15} />, active: true },
+    { name: 'Clothing Store', icon: <ShoppingCart size={15} />, active: false },
+    { name: 'Gifts and Living', icon: <Gift size={15} />, active: false },
+    { name: 'Inspiration', icon: <Lightbulb size={15} />, active: false },
   ];
 
   const containerVariants = {
@@ -36,23 +36,33 @@ export const NavItems = () => {
 
   const itemVariants = {
     hidden: { opacity: 0, x: -40 },
-    show: { opacity: 1, x: 0, transition: { type: "spring" as const, stiffness: 80 } },
+    show: {
+      opacity: 1,
+      x: 0,
+      transition: { type: 'spring' as const, stiffness: 80 },
+    },
   };
 
   return (
     <motion.div
-      className="flex flex-col gap-3 my-4 px-4"
+      className='flex flex-col gap-3 my-4 px-4'
       variants={containerVariants}
-      initial="hidden"
-      animate="show"
+      initial='hidden'
+      animate='show'
     >
       {navItemLinks.map((item) => (
         <motion.div
           key={item.name}
           variants={itemVariants}
           className={`flex items-center gap-[10px] cursor-pointer font-medium py-2 px-4 rounded-4xl
-            ${item.active && "bg-blue-600 text-white hover:bg-blue-700 hover:text-white transition-colors"}
-            ${!item.active && "hover:bg-neutral-400 hover:text-white transition-colors"}
+            ${
+              item.active &&
+              'bg-blue-600 text-white hover:bg-blue-700 hover:text-white transition-colors'
+            }
+            ${
+              !item.active &&
+              'hover:bg-neutral-400 hover:text-white transition-colors'
+            }
           `}
         >
           {item.icon}
