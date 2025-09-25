196d88cd9e426273a503341e2cb562caca646f73
vxnshK <vkapoor0112@gmail.com>
2025-09-26 04:47:22 +0530

    fixes for ui components

--- components/feed/SubHeader/Filters.tsx
+++ components/feed/SubHeader/Filters.tsx
@@ -1,9 +1,12 @@
+import { ListFilter } from "lucide-react"
+
 export default function Filters() {
   return (
     <div
-      className={`flex items-center text-xs font-medium gap-[4px] cursor-pointer hover:bg-blue-600 text-white bg-blue-500 transition-all shadow-sm px-4 py-1 rounded-4xl`}
+      className={`flex items-center gap-[8px] text-xs font-medium cursor-pointer text-white hover:bg-gray-700 bg-gray-600 transition-all shadow-sm px-4 py-3 rounded-4xl`}
     >
-      Filters
+      <ListFilter size={15} />
+      <span>Filters</span>
     </div>
   );
 }
--- components/feed/SubHeader/ScreenCategories.tsx
+++ components/feed/SubHeader/ScreenCategories.tsx
@@ -8,18 +8,18 @@ export default function ScreenCategories() {
   ];
 
   return (
-    <div className='flex gap-4 items-center overflow-hidden'>
+    <div className='flex gap-4 items-center'>
       {screenCategoriesList.map((item) => (
-        <div key={item.name}>
+        <div key={item.name} className='relative'>
           <div
             className={`flex items-center gap-[4px] cursor-pointer ${
-              item.active ? 'text-black' : 'text-gray-500 opacity-90'
-            } hover:text-black hover:bg-gray-200 bg-gray-100 transition-all shadow-sm px-4 py-1 rounded-4xl mb-2`}
+              item.active ? 'text-black' : 'text-gray-500 opacity-75'
+            } hover:text-black hover:bg-gray-200 bg-gray-100 transition-all shadow-sm px-4 py-1 rounded-4xl`}
           >
             {item.icon}
             <span className='text-xs font-bold'>{item.name}</span>
           </div>
-          {item.active && <hr className='w-5 h-1 bg-blue-400 mx-auto' />}
+          {item.active && <hr className='w-8 h-1 bg-blue-500 absolute -bottom-6.5 left-[50%] -translate-x-[50%]' />}
         </div>
       ))}
     </div>
