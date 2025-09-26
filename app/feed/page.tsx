1f6f3806c7dccfaf1d9dc767ac1a4d7d424fa09e
vxnshK <vkapoor0112@gmail.com>
2025-09-26 12:01:42 +0530

    init grid bg

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -4,14 +4,20 @@ import SubHeader from '@/components/feed/SubHeader/SubHeader';
 
 function page() {
   return (
-    <div className='flex border border-gray-300 rounded-2xl h-[95vh] w-[97vw] overflow-hidden drop-shadow-2xl absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-      <Sidebar />
-      <div className='flex-4'>
-        <Header />
-        <SubHeader />
+    <div className="bg-[url('/grid_bg.jpg')] w-screen h-screen bg-contain">
+      <div className="absolute inset-0 bg-white/60"></div>
+      <div className='z-1 flex border border-gray-300 rounded-2xl bg-white h-[92vh] w-[95vw] overflow-hidden shadow-lg absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+        <Sidebar />
+        <div className='flex-4'>
+          <div className='border-b-[1px] border-gray-300'>
+            <Header />
+            <SubHeader />
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
 export default page;
+
--- components/feed/SubHeader/SubHeader.tsx
+++ components/feed/SubHeader/SubHeader.tsx
@@ -4,7 +4,7 @@ import SearchBar from './SearchBar'
 
 export default function SubHeader() {
   return (
-    <div className='px-6 pt-2 pb-4 flex items-center justify-between border-b-[1px] border-gray-300'>
+    <div className='px-6 pt-2 pb-4 flex items-center justify-between'>
       <span className='text-4xl font-medium'>Explore</span>
       <ScreenCategories />
       <div className='flex items-center gap-[10px]'>
